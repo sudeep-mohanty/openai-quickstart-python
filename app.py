573f39faced63cff0854a13ff8141264b22f12fe
Sudeep Mohanty <sudp.mohanty@gmail.com>
2023-04-02 11:46:27 +0200

    Updated model to use gpt-3.5-turbo

--- app.py
+++ app.py
@@ -11,12 +11,19 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 def index():
     if request.method == "POST":
         animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "user", "content": generate_prompt(animal)}
+            ]
         )
-        return redirect(url_for("index", result=response.choices[0].text))
+        return redirect(url_for("index", result=response.choices[0].message.content))
+        #response = openai.Completion.create(
+        #    model="text-davinci-003",
+        #    prompt=generate_prompt(animal),
+        #    temperature=0.6,
+        #)
+        #return redirect(url_for("index", result=response.choices[0].text))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
